b99b3d1e1b01f2a50ff1f44c6d6a13a03b4af559
Aprendiz <Aprendiz@BOGDFPCGMFN4150.FormacionSena.RED>
2024-02-27 15:25:56 -0500

    entidades

--- app/models.py
+++ app/models.py
@@ -12,3 +12,14 @@ class Medico(db.Model):
     numero_identificacion = db.Column(db.Integer)
     registro_medico = db.Column(db.Integer)
     especialidad = db.Column(db.String(50))
+    
+class paciente(db.Model):
+    
+    id = db.Column(db.Integer, primary_key = True ) 
+    nombres = db.Column(db.String(120), nullable = True )
+    apellidos = db.Column(db.String(120), nullable = True)
+    tipo_identificacion = db.Column(db.String(4), nullable = True)
+    numero_identificacion = db.Column(db.Integer)
+    altura = db.Column(db.Integer)
+    tipo_sangre = db.Column(db.String(2))
+    
