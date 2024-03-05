3e63712c85f68e3c7a7d0c407c89d9bcb26c2c4f
Aprendiz <zayithuranirodriguezvargas@gmail.com>
2024-03-05 17:16:12 -0500

    citas

--- app/config.py
+++ app/config.py
@@ -1,3 +1,7 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config: 
-    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:@localhost/citas_2826502'
+    ##SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:@localhost/citas_2826502'
+    SQLALCHEMY_DATABASE_URI= "sqlite:///"+ os.path.join(basedir, "database.db")
     SQLALCHEMY_TRACK_NOTIFICATION=True
--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class Medico(db.Model):
     registro_medico = db.Column(db.Integer)
     especialidad = db.Column(db.String(50))
     
-    citas = db.relationship("Cita" , backref = "medico" )
+    citas = db.relationship('Cita' , backref = 'medico' )
 
 class Paciente(db.Model):
     __tablename__ ='pacientes'
@@ -25,12 +25,15 @@ class Paciente(db.Model):
     numero_identificacion = db.Column(db.Integer)
     altura = db.Column(db.Integer)
     tipo_sangre = db.Column(db.String(2))
+
+    citas = db.relationship('Cita' , backref = 'paciente' )
     
 class Consultorio(db.Model):
-    
     __tablename__ = 'consultorios'
     id= db.Column(db.Integer, primary_key = True)
     numero = db.Column(db.Integer)
+
+    citas = db.relationship('Cita' , backref = 'consultorio' )
 
 class Cita(db.Model):
     __tablename__ = 'citas'
@@ -39,4 +42,20 @@ class Cita(db.Model):
     paciente_id = db.Column(db.Integer, db.ForeignKey('pacientes.id'))
     medico_id = db.Column(db.Integer, db.ForeignKey('medicos.id'))
     consultorio_id = db.Column(db.Integer, db.ForeignKey('consultorios.id')) 
-    
+
+'''    
+med1 = Medico(nombres = 'gonzalo', apellidos = 'Fernandez', tipo_identificacion = 'CC', numero_identificacion = 123455, registro_medico = 8171635, especialidad ='cardiologo')
+med2 = Medico(nombres = 'Sandra', apellidos = 'Vargas', tipo_identificacion = 'CC', numero_identificacion = 4163839, registro_medico = 765456, especialidad ='Urologa')
+pac1 = Paciente(nombres = 'Maria', apellidos = 'Martinez', tipo_identificacion = 'CC', numero_identificacion = 234577,altura = 159, tipo_sangre = 'O+' )
+pac2 = Paciente(nombres = 'Juan', apellidos = 'Romero', tipo_identificacion = 'CC', numero_identificacion = 876568,altura = 180, tipo_sangre = 'A-' )
+pac3 = Paciente(nombres = 'Augusto', apellidos = 'villalba', tipo_identificacion = 'CC', numero_identificacion = 896383,altura = 173, tipo_sangre = 'O+' )
+con1 = Consultorio(numero = 102)
+con2 = Consultorio(numero = 203)
+con3 = Consultorio(numero = 206)
+mi_cita = Cita(fecha = '2024-03-05', paciente_id = 1, medico_id = 1, consultorio_id = 1 )
+fecha1 = datetime(2024, 03, 05)
+
+for ci in Cita.query.all():        
+...     print('fecha: '+str(ci.fecha)+' Paciente identificación: '+ str(ci.paciente.numero_identificacion)+ ',paciente nombre: ' + ci.paciente.nombres+' '+ ci.paciente.apellidos)
+
+'''    
